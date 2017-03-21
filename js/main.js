becb493e11a5c864dc0f020bfff2453398c29ad5
evgalp <evgeniyalpayev@gmail.com>
2017-03-21 22:58:47 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('asd')
+
+// constants obj - variables common to any mill type
+let costants = {
+
+}
+
+// roollingMill - mill constructor function
+
+function RollingMill(n, m){
+
+	this.n = n;
+	this.m = m;
+	this.nSquare = this.n * this.n;
+
+	return this;
+}
+
+let mill15_30 = new RollingMill(15, 20);
