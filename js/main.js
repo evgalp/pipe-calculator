9e8d888211737979f2151366b8871d39f45d0da9
evgalp <evgeniyalpayev@gmail.com>
2017-04-01 00:32:55 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-// globalResult - obj to write all values calculated in functions
 
-var globalResult = {
 
-}
+var rollSize = {};
+var guidePlane = {};
 
 // RollingMill - mill constructor function
 
-function RollingMill(baseRollDiameter, sigmaHalf, alpha, n, carriageStrokeLength,diametersMaxRelation, L, carriageStrokeLengthRotational, reductionSectionLength, trunnionDiameter){
+function RollingMill(baseRollDiameter, sigmaHalf, alpha, n, carriageStrokeLength,diametersMaxRelation, L, carriageStrokeLengthRotational, reductionSectionLength, trunnionDiameter, DkDcMax){
 
 	this.baseRollDiameter = baseRollDiameter;
 	this.sigmaHalf = sigmaHalf;
@@ -19,6 +18,7 @@ function RollingMill(baseRollDiameter, sigmaHalf, alpha, n, carriageStrokeLength
 	this.carriageStrokeLengthRotational = carriageStrokeLengthRotational;
 	this.reductionSectionLength = reductionSectionLength;
 	this.trunnionDiameter = trunnionDiameter;
+	this.DkDcMax = DkDcMax;
 
 	return this;
 }
@@ -43,7 +43,7 @@ function toRadians (angle) {
 
 // declare variables - mills
 
-var millOne =  new RollingMill(82, 0.5, 60, 3, 455, 1.6, 210, 69, 12, 45);
+var millOne =  new RollingMill(82, 0.5, 60, 3, 455, 1.6, 210, 69, 12, 45, 1.6);
 
 // declare variables - route
 
@@ -53,18 +53,23 @@ var routeOne = new Route(17.5, 16.3, 0.7, 0.35);
 
 function calcRollSize(mill, route){
 
-	globalResult.rebordDiameter = (mill.baseRollDiameter - mill.sigmaHalf) * Math.sin(mill.alpha);
+	rollSize.rebordDiameter = (mill.baseRollDiameter - mill.sigmaHalf) * Math.sin(mill.alpha);
 
-	globalResult.bottomRollDiameter = mill.baseRollDiameter - route.billetDiameterFinal;
+	rollSize.bottomRollDiameter = mill.baseRollDiameter - route.billetDiameterFinal;
 
-	globalResult.rebordMinimalThickness = 0.7 * (route.billetDiameterFinal / 2) * (1 - Math.cos(mill.alpha));
+	rollSize.rebordMinimalThickness = 0.7 * (route.billetDiameterFinal / 2) * (1 - Math.cos(mill.alpha));
 
 }
 
 function calcGuidePlaneSize(mill, route){
 
-	globalResult.effectiveRollDiameter = globalResult.bottomRollDiameter - route.billetDiameterFinal;
+	guidePlane.effectiveRollDiameter = rollSize.bottomRollDiameter + 0.2 * route.billetDiameterFinal;
+
+	guidePlane.guidePlaneWorkLength = (mill.carriageStrokeLength) / (1 + mill.DkDcMax);
+
+	guidePlane.lb = mill.L - guidePlane.guidePlaneWorkLength;
 	
+	guidePlane.ln = mill.carriageStrokeLengthRotational / (1 + mill.DkDcMax)
 	
 }
 
@@ -74,7 +79,10 @@ calcGuidePlaneSize(millOne, routeOne);
 
 console.log(millOne);
 
-console.log(globalResult);
+console.log(rollSize);
+
+console.log(guidePlane);
+
 
 // calcRollSize(millTwo, routeOne);
 // calcGuidePlaneSize(millTwo, routeOne);
